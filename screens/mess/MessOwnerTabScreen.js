ebca1b69080bdb23322e2e5950f571e71473767c
vishalraoPVR <rvishal757@gmail.com>
2023-04-23 00:38:59 +0530

    Update munu

--- screens/mess/MessOwnerTabScreen.js
+++ screens/mess/MessOwnerTabScreen.js
@@ -1,18 +1,64 @@
 import HomeScreen from "./HomeScreen";
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import ProfileScreen from "./ProfileScreen";
+import Icon from "react-native-vector-icons/Ionicons";
+import {View,StyleSheet,Button} from 'react-native';
+
+import {
+  Avatar,
+  Title,
+  Caption,
+  Paragraph,
+  Text,
+  TouchableRipple,
+  Switch,
+  
+} from 'react-native-paper'
+
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Home } from "@mui/icons-material";
+const Drawer = createDrawerNavigator();
+
+import UpdateMenu from "./UpdateMenu";
+import MessDetails from "./MessDetails";
+import CustomerList from "./CustomerList";
 
 const Tab = createMaterialBottomTabNavigator();
 
+
 function MessOwnerTabScreen(){
     return(
-      <Tab.Navigator
-        screenOptions={{headerShown:false}}
-      >
-        <Tab.Screen name='Home' component={HomeScreen}/>
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-      </Tab.Navigator>
+      <Drawer.Navigator>
+      <Drawer.Screen name="Home" component={HomeScreen} 
+      options={{
+        drawerIcon:({ focused }) => (
+          <Icon name="home" />
+        )
+      }}
+      />
+      <Drawer.Screen name="profile" component={ProfileScreen} 
+      options={{
+        drawerIcon:({ focused }) => (
+          <Icon name="person-circle-outline" />
+        )
+      }}
+      />
+      <Drawer.Screen name="Update Menu" component={UpdateMenu}
+      options={{
+        drawerIcon:({ focused }) => (
+         <></>
+        )
+      }}
+      />
+      <Drawer.Screen name="Mess Details" component={MessDetails}/>
+      <Drawer.Screen name="Customers" component={CustomerList} />
+
+    </Drawer.Navigator>
     );
   }
+
+  const styles =StyleSheet.create({
+
+  });
 
   export default MessOwnerTabScreen
