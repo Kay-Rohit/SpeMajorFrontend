97b81d729406127c1f0b603f652b3e68181cbed8
Kantimahanty Rohit <61279841+Kay-Rohit@users.noreply.github.com>
2023-04-25 15:39:52 +0530

    base urls for handling requests in docker

--- assets/URL.js
+++ assets/URL.js
@@ -1,4 +1,4 @@
 //process.env is used to fetch the environment variable of the system and thus fetch the ip
 
 const url = process.env.HOST_IP;
-export const baseUrl = `http://a7ce-2401-4900-16eb-a72f-3e1f-a64c-c25a-3914.ngrok-free.app`;
+export const baseUrl = `http://${url}:5000`;
